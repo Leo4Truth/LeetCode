5ebe5bb24725646aaea780bd06b76c53a1999966
liuyuwen <liuyuwen@baidu.com>
2021-09-24 19:51:19 +0800

    2021.09.24

---
+++ problems/1036_EscapeALargeMaze.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <unordered_set>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isEscapePossible(vector<vector<int>>& blocked, vector<int>& source, vector<int>& target) {
+        return bfs(blocked, source, target) && bfs(blocked, target, source);
+    }
+
+    bool bfs(vector<vector<int>>& blocked, vector<int>& source, vector<int>& target) {
+        int m = blocked.size();
+        /**
+         * o o o o o o o x o o o o o
+         * o o o o o o x o o o o o o
+         * o o o o o x o o o o o o o
+         * o o o o x o o o o o o o o
+         * o o o x o o o o o o o o o
+         * o o x o o o o o o o o o o
+         * o x o o o o o o o o o o o
+         * x o o o o o o o o o o o o
+         */
+        int max_area = m * m / 2 + 1;
+
+        unordered_set<int> blocked_set;
+        for (int i = 0; i < m; i++) {
+            int x = blocked[i][0];
+            int y = blocked[i][1];
+            blocked_set.insert(xy(x, y));
+        }
+
+        unordered_set<int> visited;
+        visited.insert(xy(source[0], source[1]));
+
+        queue<pair<int, int>> q;
+        q.push({source[0], source[1]});
+
+        while (!q.empty()) {
+            auto [curr_x, curr_y]  = q.front();
+            q.pop();
+            for (auto dir : dirs) {
+                int next_x = curr_x + dir[0];
+                int next_y = curr_y + dir[1];
+                if (next_x < 0 || next_x >= N || next_y < 0 || next_y >= N 
+                    || blocked_set.count(xy(next_x, next_y)) 
+                    || visited.count(xy(next_x, next_y))) {
+                    continue;
+                }
+                visited.insert(xy(next_x, next_y));
+                q.push({next_x, next_y});
+                // visit target or get out of the max area that m blocked squares can cover
+                if (next_x == target[0] && next_y == target[1] || visited.size() > max_area) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+private:
+    const int N = 1e6;
+    int dirs[4][2] = {
+        {0, 1}, {0, -1}, {-1, 0}, {1, 0}
+    };
+
+    /**
+     * @brief turn (x, y) pair coordinate into xy single long integer index
+     * 
+     * @param x
+     * @param y
+     * @return long
+     */
+    long xy(long x, long y) {
+        return x * N + y;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    Solution solution;
+
+    return 0;
+}
--- problems/300_LongestIncreasingSubsequence.cpp
+++ problems/300_LongestIncreasingSubsequence.cpp
@@ -11,15 +11,75 @@ using namespace std;
 
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
+    // Greedy, we hope taht the sequence grows larger as slow as possible
+    // so we hope that the last element of the ascending sequence is as small as possible
+    // d[i] represents the smallest last element in ascending sequence with length i
+    // len records the length of the ascending sequence length
+    // d[i] is monotonically increasing
+    int lengthOfLIS_greedy_binary(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
 
+        int len = 1;
+        vector<int> d(n + 1, 0);
+        d[len] = nums[0];
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > d[len]) {
+                len++;
+                d[len] = nums[i];
+            } else {
+                // binary search d[1...len] to find the smallest
+                int l = 1, r = len;
+                int pos = 0; // 如果找不到说明所有的数都比 nums[i] 大，此时要更新 d[1]，所以这里将 pos 设为 0
+                while (l <= r) {
+                    int mid = (l + r) / 2;
+                    if (d[mid] < nums[i]) {
+                        pos = mid; // d[1...mid] < nums[i], 所以从 pos + 1 = mid + 1 开始更新 d[pos + 1] = nums[i]
+                        l = mid + 1;
+                    } else {
+                        r = mid - 1;
+                    }
+                }
+                d[pos + 1] = nums[i];
+            }
+        }
+        
+        return len;
+    }
+
+    // dp
+    // Time Complexity: O(N^2)
+    // Space Complexity: O(N)
+    int lengthOfLIS_dp(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 0) return 0;
+
+        int max_len = 1;
+        vector<int> dp(n, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = dp[j] + 1 > dp[i] ? dp[j] + 1 : dp[i];
+                    max_len = dp[i] > max_len ? dp[i] : max_len;
+                }
+            }
+        }
+        
+        return max_len;
     }
 };
 
 int main(int argc, char* argv[]) {
+    int n = argc >= 2 ? stoi(argv[1]) : 0;
+    vector<int> nums;
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        nums.push_back(num);
+    }
 
     Solution solution;
-
-    //system("pause");
+    cout << solution.lengthOfLIS_greedy_binary(nums) << endl;
+    
     return 0;
 }
