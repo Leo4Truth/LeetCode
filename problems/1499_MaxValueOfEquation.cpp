e443a09f2a05b2d3ea7c3f61e604b3f8f03872f7
Yuwen Liu <lyw.765486562@outlook.com>
2021-10-21 00:09:11 +0800

    2021.10.20 1499 completed, more solutions to explore

--- problems/1499_MaxValueOfEquation.cpp
+++ problems/1499_MaxValueOfEquation.cpp
@@ -1,30 +1,50 @@
 #include <iostream>
 #include <vector>
-#include <stdint.h>
+#include <queue>
 
 using namespace std;
 
+struct Point {
+    int x;
+    int y;
+    bool operator<(const Point &other) const {
+        return (y - x) < (other.y - other.x);
+    }
+    int operator-(const Point &other) const {
+        return y + other.y + other.x - x;
+    }
+    Point(const vector<int>& p): x(p[0]), y(p[1]) {}
+};
+
 class Solution {
 public:
+    // priority queue
+    // max(yi + yj + |xi - xj|) = max(yi + yj + xj - xi) = max(xj + yj) - min(xi - yi)
     int findMaxValueOfEquation(vector<vector<int>>& points, int k) {
-        int n = points.size();
-        if (n == 0 || n == 1) return 0;
-        int max = INT32_MIN;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                int dx = abs(points[i][0] - points[j][0]) <= k
-                if (dx) {
-                    if ()
-                }
-            }
+        priority_queue<Point> pq;
+        int res = INT32_MIN;
+        for (auto &p: points) {
+            Point po(p);
+            while (pq.size() && (po.x - pq.top().x > k)) pq.pop();
+            if (pq.size()) res = std::max(res, pq.top() - po);
+            pq.push(po);
         }
+        return res;
     }
 };
 
 int main(int argc, char* argv[]) {
+    int n = argc >= 2 ? stoi(argv[1]) : 0;
+    int k = argc >= 3 ? stoi(argv[2]) : 0;
+    vector<vector<int>> points;
+    for (int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        points.push_back({x, y});
+    }
 
     Solution solution;
-
-    //system("pause");
+    cout << solution.findMaxValueOfEquation(points, k) << endl;
+    
     return 0;
 }
