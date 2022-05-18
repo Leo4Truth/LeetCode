2ec8bdc40f6361ed5e1bcfd39ddd3a8c271706be
Leo4Truth <lyw.765486562@outlook.com>
2022-05-18 23:32:02 +0800

    2022-05-18 668

---
+++ problems/668_KthSmallestNumberInMultiplicationTable.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    virtual int findKthNumber(int m, int n, int k) = 0;
+};
+
+class Solution_init : public Solution {
+public:
+    int findKthNumber(int m, int n, int k) {
+        int left = 1, right = m * n;
+        while (left < right) {
+            int x = left + (right - left) / 2;
+            int count = x / n * n;
+            for (int i = x / n + 1; i <= m; ++i) {
+                count += x / i;
+            }
+            if (count >= k) {
+                right = x;
+            } else {
+                left = x + 1;
+            }
+        }
+        return left;
+    }
+
+private:
+};
+
+int main(int argc, char* argv[]) {
+    int m = argc >= 2 ? stoi(argv[1]) : 0;
+    int n = argc >= 3 ? stoi(argv[2]) : 0;
+    int k = argc >= 4 ? stoi(argv[3]) : 0;
+
+    Solution *solution = nullptr;
+
+    solution = new Solution_init();
+    cout << solution->findKthNumber(m, n, k) << endl;
+
+    return 0;
+}
