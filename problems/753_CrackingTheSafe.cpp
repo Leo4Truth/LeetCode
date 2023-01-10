8605d5fa07f9fbd64595b943841b4946993ff9c9
Leo4Truth <lyw.765486562@outlook.com>
2023-01-10 08:16:39 +0800

    2022-01-10 753 diffcult done

---
+++ problems/753_CrackingTheSafe.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+class Solution {
+public:
+    virtual string crackSafe(int n, int k) = 0;
+};
+
+class Solution_dfs : public Solution {
+public:
+    string crackSafe(int n, int k) {
+        highest = pow(10, n - 1);
+        this->k = k;
+        dfs(0);
+        ans += string(n - 1, '0');
+        return ans;
+    }
+
+private:
+    unordered_set<int> seen;
+    string ans;
+    int highest;
+    int k;
+
+    void dfs(int node) {
+        for (int x = 0; x < k; ++x) {
+            int nei = node * 10 + x;
+            if (!seen.count(nei)) {
+                seen.insert(nei);
+                dfs(nei % highest);
+                ans += (x + '0');
+            }
+        }
+    }
+};
+
+int main(int argc, char* argv[]) {
+    int n = argc >= 2 ? stoi(argv[1]) : 0;
+    int k = argc >= 3 ? stoi(argv[2]) : 0;
+
+    Solution *solution = nullptr;
+
+    solution = new Solution_dfs();
+    cout << solution->crackSafe(n, k) << endl;
+
+    return 0;
+}
